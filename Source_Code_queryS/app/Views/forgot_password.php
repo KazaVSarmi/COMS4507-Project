c68d61bc08139e1280f6c912127015f98ad8f732
Gabriel Brits <gabriel.brits@icloud.com>
2024-04-22 10:07:30 +1000

    rm Source_Code

--- Source_Code_queryS/app/Views/forgot_password.php
+++
@@ -1,9 +0,0 @@
-<h2>Forgot password?</h2>
-<p>Use this form to recover your account</p>
-<?php echo form_open(base_url() . 'forgot_password/request_email'); ?>
-<p>Enter the username of the account you want to recover</p>
-<input type="text" name="username" value='' required="required">
-<p>Enter the email that you used to register</p>
-<input type="text" name="email" value='' required="required">
-<button type="submit" name="submit_button">Submit</button>
-<?php echo form_close(); ?>
--- Source_Code_queryS/app/Views/forgot_password_verify.php
+++
@@ -1,7 +0,0 @@
-<p>Enter the 6 digit verification code that you received. </p>
-<?php echo form_open(base_url().'forgot_password/verify_code'); ?>
-<p>You requested a code for the username:</p>
-<input type="text" name="username" value=<?php echo $username; ?> readonly>
-<input type="text" name="verification_key" value='' required="required">
-<button type="submit" name="submit_button">Submit</button>
-<?php echo form_close(); ?>
